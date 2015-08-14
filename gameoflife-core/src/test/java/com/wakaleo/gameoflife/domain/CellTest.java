49d96cdc8316e7bc8e0b4e73d85d16828ad29f45
Zhou Qing <qing.zhou@rea-group.com>
2015-08-14 20:15:22 +0800

    Rename test for try to solve coverage problem

--- gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/CellTest.java
+++ gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/CellTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
 
-public class WhenYouCreateACell {
+public class CellTest {
 
     @Test
     public void aLiveCellShouldBeRepresentedByAnAsterisk() {
